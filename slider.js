8752e66b6fa99fe15626c643735f538289333705
Iogsotot <ajustusa@gmail.com>
2020-07-10 14:26:31 +0300

    clean slider.js

--- slider.js
+++ slider.js
@@ -2,12 +2,10 @@ let slides = document.querySelector(".video__slider__items").children;
 let prevSlide = document.querySelector(".slider__control--left");
 let nextSlide = document.querySelector(".slider__control--right");
 let totalSlides = slides.length;
-let index = 0; 
 
 function setStartOrder() {
     for (let i = 0; i < totalSlides; i++) {
         slides[i].style.order = i+1;
-        console.log("слайд № " + i + " имеет order - " + slides[i].style.order);
     }
 }
 
@@ -26,17 +24,13 @@ function sliding(direction) {
             slides[j].style.order++ ;
             if (slides[j].style.order > totalSlides) {
                 slides[j].style.order = 1;
-                console.log("мы дошли до края!")
             }
-            console.log("NEW! слайд № " + j + " имеет order - " + slides[j].style.order)
         }
         else if (direction == "next") {
             slides[j].style.order-- ;
             if (slides[j].style.order < 1) {
                 slides[j].style.order = totalSlides;
-                console.log("мы дошли до края!")
             }
-            console.log("NEW! слайд № " + j + " имеет order - " + slides[j].style.order)
         }
     }    
 }
